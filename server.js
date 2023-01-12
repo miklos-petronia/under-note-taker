2e6b3fcc9de57931a9c6caf7d2e21569ef7f8520
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-01-12 15:41:26 +0100

    updated server js file

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
+// Dependencies
 const express = require('express');
 
+// Developing environment variable port
 const PORT = process.env.PORT || 3001;
+
+// Application usage express
 const app = express();
+
+// Routes to route files
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-// Parse URL encoded & JSON
+// Determine URL encoded & JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Host public folder
+// Host of public file
 app.use(express.static('public'));
 
-// Use apiRoutes
+// Usage of apiRoutes
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
+// Application listener and it commence the server
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
